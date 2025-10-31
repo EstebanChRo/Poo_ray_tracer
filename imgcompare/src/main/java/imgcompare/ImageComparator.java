370800389e0bffcc4961f0cc7139cd49d89c0633
Esteban <este8617@gmail.com>
2025-10-31 14:04:18 +0100

    méthode compteur_pixel_diff

--- imgcompare/src/main/java/imgcompare/ImageComparator.java
+++ imgcompare/src/main/java/imgcompare/ImageComparator.java
@@ -1,5 +1,22 @@
 package imgcompare;
 
+import java.awt.image.BufferedImage;
+
 public class ImageComparator {
     
+    public int compteur_pixel_diff(BufferedImage Img1, BufferedImage Img2){
+        int compteur = 0;
+        for (int x=0; x < Img1.getWidth(); x++){
+            for (int y=0; y < Img1.getHeight(); y++){
+                if (Img1.getRGB(x,y) == Img2.getRGB(x,y)){
+                    compteur++;
+                }
+            }
+        }
+        return compteur;
+    }
+
+    
+
+
 }
