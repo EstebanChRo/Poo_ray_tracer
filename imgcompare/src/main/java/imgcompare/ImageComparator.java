b817a73545d13659dbb7456301840eac0c39fc93
Esteban <este8617@gmail.com>
2025-10-31 16:34:23 +0100

    Correction compteur_pixel_diff et fonction BufferedImage

--- imgcompare/src/main/java/imgcompare/ImageComparator.java
+++ imgcompare/src/main/java/imgcompare/ImageComparator.java
@@ -3,12 +3,11 @@ package imgcompare;
 import java.awt.image.BufferedImage;
 
 public class ImageComparator {
-    
     public int compteur_pixel_diff(BufferedImage Img1, BufferedImage Img2){
         int compteur = 0;
         for (int x=0; x < Img1.getWidth(); x++){
             for (int y=0; y < Img1.getHeight(); y++){
-                if (Img1.getRGB(x,y) == Img2.getRGB(x,y)){
+                if (Img1.getRGB(x,y) != Img2.getRGB(x,y)){
                     compteur++;
                 }
             }
@@ -16,7 +15,15 @@ public class ImageComparator {
         return compteur;
     }
 
-    
-
-
+    public BufferedImage img_différentielle(BufferedImage Img1, BufferedImage Img2){
+        BufferedImage img_différentielle = new BufferedImage(Img1.getWidth(), Img1.getHeight(),BufferedImage.TYPE_INT_RGB);
+        for (int x=0; x < Img1.getWidth(); x++){
+            for (int y=0; y < Img1.getHeight(); y++){
+                int pixel1=Img1.getRGB(x, y);
+                int pixel2=Img2.getRGB(x, y);
+                img_différentielle.setRGB(x, y, pixel1 ^ pixel2);
+            }
+        }
+        return img_différentielle;
+    }
 }
