8289603c5e8568731694ac31b9eb703e35b569e7
Esteban <este8617@gmail.com>
2025-11-11 13:46:50 +0100

    ajout validateurs dans Vector.java + corrige quelques erreurs

--- ray_tracer/src/main/java/ray_tracer/Vector.java
+++ ray_tracer/src/main/java/ray_tracer/Vector.java
@@ -7,11 +7,17 @@ public class Vector extends AbstractVec3 {
     }
 
     public Vector add(AbstractVec3 other){
+        if (!(other instanceof Vector)) {
+            throw new IllegalArgumentException("A Vector can only be added to another Vector.");
+        }
         Vector v = (Vector) other;
         return new Vector(this.x + v.x, this.y + v.y, this.z + v.z);
     }
 
-    public Vector subtract (AbstractVec3 other){
+    public Vector subtract(AbstractVec3 other){
+        if (!(other instanceof Vector)) {
+            throw new IllegalArgumentException("A Vector can only be subtracted from another Vector.");
+        }
         Vector v = (Vector) other;
         return new Vector(this.x - v.x, this.y - v.y, this.z - v.z);
     }
@@ -20,19 +26,29 @@ public class Vector extends AbstractVec3 {
         return new Vector(this.x * scalar, this.y * scalar, this.z * scalar);
     }
 
-    public double produitScalaire(Vector other){
+    public double dotProduct(Vector other){
+        if (other == null) {
+            throw new IllegalArgumentException("The argument cannot be null.");
+        }
         return this.x * other.x + this.y * other.y + this.z * other.z;
     }
 
-    public Vector produitVectoriel(Vector other){
-        return new Vector(this.y * other.z - this.z * other.y, this.z * other.x - this.x * other.z , this.x + other.y - this.y * other.x);
+    public Vector crossProduct(Vector other){
+        if (other == null) {
+            throw new IllegalArgumentException("The argument cannot be null.");
+        }
+        return new Vector(this.y * other.z - this.z * other.y, this.z * other.x - this.x * other.z , this.x * other.y - this.y * other.x);
     }
 
     public double length(){
         return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
     }
 
-    public Vector normalize(){
-        return new Vector(this.x / length(), this.y / length(), this.z / length());
+    public Vector normalize() {
+        double len = this.length();
+        if (len == 0.0) {
+            throw new ArithmeticException("Cannot normalize a vector with zero length.");
+        }
+        return new Vector(this.x / len, this.y / len, this.z / len);
     }
 }
