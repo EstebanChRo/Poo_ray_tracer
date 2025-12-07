689b2a8a0761917256f540cd85a824002ac34d0d
Esteban <este8617@gmail.com>
2025-12-07 14:15:18 +0100

    teste main

--- ray_tracer/src/main/java/ray_tracer/main.java
+++ ray_tracer/src/main/java/ray_tracer/main.java
@@ -16,21 +16,4 @@ public class main {
         ImageWriter writer = new ImageWriter();
         writer.write(image, scene.getOutput());
     }
-/*
-TODO : Jalon-4 : Étape 5 : Tester la réflexion diffuse
-Objectif : Valider que le modèle de Lambert est correctement implémenté.
-Tests suggérés :
-
-Test unitaire :
-    Vérifier que Intersection.calculateDiffuseColor retourne :
-        Une couleur noire si la lumière est parallèle à la surface (produit scalaire = 0).
-        Une couleur maximale si la lumière est perpendiculaire à la surface (produit scalaire = 1).
-
-Test visuel :
-    Générer une image avec une sphère et une lumière ponctuelle.
-    Vérifier que :
-        Le côté de la sphère face à la lumière est plus clair.
-        Le côté opposé est plus sombre (seulement lumière ambiante).
-*/
-
 }
